1935a75f8d179ee28ed87aa90dafa5ceddeea8ad
cassidywalsh <cassidy.walsh19@blstudent.org>
2018-07-25 19:43:24 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -5,7 +5,7 @@
 
 def add_a_key_value_pair
  olympics = {:sydney => 2000, :athens => 2004, :beijing => 2008, :london => 2012}
- olympics[:atlanta => 1996]
+ olympics[:atlanta] = 1996
  puts olympics
 end
 
