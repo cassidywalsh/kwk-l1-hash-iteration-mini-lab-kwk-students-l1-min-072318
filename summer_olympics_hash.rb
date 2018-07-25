3e6aaca217cc711d0f0858201960176779f7133d
cassidywalsh <cassidy.walsh19@blstudent.org>
2018-07-25 19:42:27 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -6,8 +6,8 @@
 def add_a_key_value_pair
  olympics = {:sydney => 2000, :athens => 2004, :beijing => 2008, :london => 2012}
  olympics[:atlanta => 1996]
+ puts olympics
 end
-puts olympics
 
 # def iterate_through_hash
 #   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
