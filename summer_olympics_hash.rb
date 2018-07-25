d0a5aca53aac4efeeb1363f9200ff82c9acebc8f
cassidywalsh <cassidy.walsh19@blstudent.org>
2018-07-25 19:51:49 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -16,7 +16,8 @@ def iterate_through_hash
   olympics.each do |x,y|
   puts "The #{y} summer olympics took place in #{x}."
 end
- iterate_through_hash
+
+iterate_through_hash
  
 # def iterate_through_keys
 #   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
